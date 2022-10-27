8ac22b3430b97afbb9b988a203220f307cc6cdf5
NguyenThiHongLinh <20130306@st.hcmuaf.edu.vn>
2022-10-27 12:30:38 +0700

    tạo nút back to top

--- demo/js/main.js
+++ demo/js/main.js
@@ -559,7 +559,20 @@ $(document).ready(function(){
         });
       }
 
+// js back to top
+    window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+        if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
+    }
 
+    document.getElementById('myBtn').addEventListener("click", function(){
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
   
 
 
