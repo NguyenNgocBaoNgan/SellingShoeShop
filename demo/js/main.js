97b7f31f88c8fb01dfc7eae84074affdea63a001
NguyenThiHongLinh <20130306@st.hcmuaf.edu.vn>
2022-11-05 22:40:12 +0700

    đổi btt

--- demo/js/main.js
+++ demo/js/main.js
@@ -560,18 +560,33 @@ $(document).ready(function(){
       }
 
 // js back to top
-    window.onscroll = function() {scrollFunction()};
-    function scrollFunction() {
-        if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-            document.getElementById("myBtn").style.display = "block";
-        } else {
-            document.getElementById("myBtn").style.display = "none";
-        }
-    }
+    jQuery(document).ready(function($){
+    // browser window scroll (in pixels) after which the "back to top" link is shown
+        var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+            offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+            scroll_top_duration = 700,
+    //grab the "back to top" link
+            $back_to_top = $('.cd-top');
 
-    document.getElementById('myBtn').addEventListener("click", function(){
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
+    //hide or show the "back to top" link
+        $(window).scroll(function(){
+            ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+            if( $(this).scrollTop() > offset_opacity ) {
+                $back_to_top.addClass('cd-fade-out');
+            }
+        });
+
+    //smooth scroll to top
+        $back_to_top.on('click', function(event){
+            event.preventDefault();
+            $('body,html').animate({
+                    scrollTop: 0 ,
+                }, scroll_top_duration
+            );
+        });
+
     });
   
 
